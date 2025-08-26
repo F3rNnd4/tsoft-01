fff3187bd04a2363db1e719f98bf04fdab6fe789
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-08-26 11:03:00 -0300

    feat: create multiplicar and dividir test

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,30 +1,61 @@
-import { somar } from '../src/calculadora.js';
-import { subtrair } from '../src/calculadora.js';
+import { somar, subtrair, multiplicar, dividir } from "../src/calculadora.js";
 
-describe('Calculadora', () => {
-    test('Deve somar dois números corretamente', () => {
-        // AAA
-        // Arrange (Preparar)
-        const a = 2;
-        const b = 3;
+describe("Calculadora", () => {
+  test("Deve somar dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+    const a = 10;
+    const b = 3;
 
-        // Act (Executar)
-        const resultado = somar(a, b);
+    // Act (Executar)
+    const resultado = somar(a, b);
 
-        // Assert (Verificar)
-        expect(resultado).toBe(5);
-    });
+    // Assert (Verificar)
+    expect(resultado).toBe(13);
+  });
 
-    test('Deve subtrair dois números corretamente', () => {
-        // AAA
-        // Arrange (Preparar)
-        const a = 5;
-        const b = 3;
+  test("Deve subtrair dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+    const a = 5;
+    const b = 3;
 
-        // Act (Executar)
-        const resultado = subtrair(a, b);
+    // Act (Executar)
+    const resultado = subtrair(a, b);
 
-        // Assert (Verificar)
-        expect(resultado).toBe(2);
-    });
+    // Assert (Verificar)
+    expect(resultado).toBe(2);
+  });
+
+  test("Deve multiplicar dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+    const a = 2;
+    const b = 3;
+
+    // Act (Executar)
+    const resultado = multiplicar(a, b);
+
+    // Assert (Verificar)
+    expect(resultado).toBe(6);
+  });
+
+  test("Deve dividir dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+    const a = 10;
+    const b = 2;
+
+    // Act (Executar)
+    const resultado = dividir(a, b);
+
+    // Assert (Verificar)
+    expect(resultado).toBe(5);
+  });
+
+  test("Deve lançar um erro ao dividir por zero", () => {
+    expect(() => {
+        dividir(10, 0);
+    }).toThrowError("Divisão por zero não é permitida.");
+  });
 });
--- src/calculadora.js
+++ src/calculadora.js
@@ -8,3 +8,16 @@ export function somar(a, b) {
 export function subtrair(a, b) {
     return a - b;
 }
+
+// Função para testar a multiplicação
+export function multiplicar(a,b) {
+    return a * b;
+}
+
+// Função para testar a divisão
+export function dividir(a, b) {
+    if (b == 0) {
+        throw new Error("Divisão por zero não é permitida.");
+    }
+    return a / b;
+}
